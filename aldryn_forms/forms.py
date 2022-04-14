8afa62b3f5cc856e5f484be0ddb08a5eb092daaf
Stefan van den Eertwegh <34129243+svandeneertwegh@users.noreply.github.com>
2022-04-14 19:22:08 +0200

    Update forms.py
    @israa djangocms added code

--- aldryn_forms/forms.py
+++ aldryn_forms/forms.py
@@ -33,9 +33,37 @@ class FileSizeCheckMixin(object):
         return data
 
 
-class RestrictedFileField(FileSizeCheckMixin, forms.FileField):
-    pass
-
+class RestrictedFileField(forms.FileField):
+    """
+    Same as FileField, but you can specify:
+    * content_types - list containing allowed content_types. Example: ['application/pdf', 'image/jpeg']
+    * max_upload_size - a number indicating the maximum file size allowed for upload.
+        2.5MB - 2621440
+        5MB - 5242880
+        10MB - 10485760
+        20MB - 20971520
+        50MB - 5242880
+        100MB - 104857600
+        250MB - 214958080
+        500MB - 429916160
+"""
+    def __init__(self, *args, **kwargs):
+        self.content_types = kwargs.pop("content_types")
+        self.max_upload_size = kwargs.pop("max_upload_size")
+        super(RestrictedFileField, self).__init__(*args, **kwargs)
+    def clean(self, data, initial=None):
+        file = super(RestrictedFileField, self).clean(data, initial)
+        try:
+            content_type = file.content_type
+            if content_type in self.content_types:
+                if file._size > self.max_upload_size:
+                    raise ValidationError(_('Please keep filesize under %s. Current filesize %s') % (
+                        filesizeformat(self.max_upload_size), filesizeformat(file._size)))
+            else:
+                raise ValidationError(_('Filetype not supported.'))
+        except AttributeError:
+            pass
+        return data
 
 class RestrictedImageField(FileSizeCheckMixin, forms.ImageField):
 
