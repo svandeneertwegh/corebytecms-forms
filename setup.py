622846eb4eae2fbc4559d373fd748bd0fcf20f6b
Stefan van den Eertwegh <stefan@corebyte.nl>
2022-11-26 15:27:21 +0100

    upload test

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     description='Create forms and embed them on CMS pages',
     long_description=open('README.rst').read(),
     long_description_content_type="text/x-rst",
-    packages=find_packages(exclude=['tests']),
+    packages=find_packages('cms_forms'),
     include_package_data=True,
     zip_safe=False,
     keywords='django django-cms form-builder form fields',
